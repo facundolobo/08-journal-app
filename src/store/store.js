b8edf93e740bfad158fb253014f0593a50d23f8e
Facundo Lobo <facundolobo2@gmail.com>
2021-03-08 18:01:10 -0300

    arreglando la imagen

--- src/store/store.js
+++ src/store/store.js
@@ -8,6 +8,6 @@ const reducers= combineReducers({
 
 //importar esto en el punto mas alto de la app abajo de index
 export const store = createStore(
-    reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        reducers,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     ); //la funcion "createStore" solo recibe un reducer 
